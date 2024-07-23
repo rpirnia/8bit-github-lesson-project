edc0312080cb93e3c4a18c2ffbebbc8a2430310a
Ruhi Pirnia <ruhiyyih@Ruhis-MacBook-Air.local>
2024-07-22 23:24:56 -0700

    update readme, add logic for hello/bye world and foobar

---
+++ FooBar.java
@@ -0,0 +1,7 @@
+public class FooBar{
+
+    public static void main(String[] args) {
+        System.out.println("FooBar"); 
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+A simple Java program that prints hello world
